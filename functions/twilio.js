cf9195735c4462ea755c156e128ae6a8457bd699
Erce Kal <erce@futr.ai>
2020-05-04 15:51:40 +0100

    first commit

---
+++ functions/twilio.js
@@ -0,0 +1,6 @@
+const twilio = require('twilio')
+
+const accountSid = 'ACb40f20df1b9341fb5914ab60c941e128'
+const authToken = '845614d7bcd5322ca882268c4662a1f3'
+
+module.exports = new twilio.Twilio(accountSid, authToken)
